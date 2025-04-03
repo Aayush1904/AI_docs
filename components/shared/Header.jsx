f8d28f054c1bb30037a37055efd8028d390e85e3
Aayush1904 <asagrawal612@gmail.com>
2025-04-03 10:15:59 +0530

    Made changes in UI for Github Content

--- components/shared/Header.jsx
+++ components/shared/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex items-center justify-between h-18'>
         <Link href="/" className='flex items-center'>
           {/* <Logo/> */}
-          <Image src = "/Logo.jpg" alt='Logo' height={150} width={150} />
+          <Image src = "/Logo_Neural.jpg" alt='Logo' height={150} width={150} />
         </Link>
 
         <SignedIn>
